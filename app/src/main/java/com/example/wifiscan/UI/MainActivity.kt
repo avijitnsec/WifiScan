d75acb5ee237c131bd173ac65f8fe9bb75791fba
Avijit Das <avijit.k.das@medtronic.com>
2020-09-04 01:31:51 +0530

    First commit

---
+++ app/src/main/java/com/example/wifiscan/UI/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.wifiscan.UI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.wifiscan.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
